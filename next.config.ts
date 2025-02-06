773765185646240d528abf2b74e8f99f5a6d0230
t4tarzan <rao.vinu@gmail.com>
2025-02-06 11:31:49 +0530

    Update port configuration to use 5001

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {},
+  env: {
+    PORT: '5001'
+  }
+}
 
 export default nextConfig;
